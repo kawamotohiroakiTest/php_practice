67233c1a40d0a686d47267551afa4f2cb71723d7
kawamotohiroakiTest <kagoshimasyusyoku@gmail.com>
2021-05-23 22:10:00 +0900

    取得したデータを表示

--- db/db_sql.php
+++ db/db_sql.php
@@ -4,11 +4,11 @@ require_once 'db_connect.php';
 
 // 出力して、htmlに出す
 // $stml = $dbh->query("SELECT * FROM blog");
-
+// $stml = $dbh->prepare("SELECT * FROM blog");
+// $stml->execute();
 // foreach ($stml as $row) {
-//     echo $row['id'].':'.$row['title'];
+//     echo $row['title'];
 // }
-
 
 // 追加する
 // $stml = $dbh->prepare("INSERT INTO blog VALUES (null, 'ブログ', '新しいタイトルです', null, 1)");
