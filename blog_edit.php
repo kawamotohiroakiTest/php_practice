8f309661dc811777aca343851617f0e87f8d9dff
kawamotohiroakiTest <kagoshimasyusyoku@gmail.com>
2021-05-24 21:14:47 +0900

    戻す

---
+++ blog_edit.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'db/db_sql.php';
+?>
+
+<form action='blog_edit_done.php' method='POST'>
+    <p>ブログタイトル<input type='text' name='title' size='191'></p>
+    <p>投稿内容<input type='text' name='content' size='5000'></p>
+    <input type='submit' value='送信'><input type='reset' value='リセット'>
+</form>
--- blog_main.php
+++ blog_main.php
@@ -3,6 +3,7 @@
 <?php
 require_once 'db/db_sql.php';
 data_display();
+
 ?>
 
 <h3><a href="blog_add.php">新規登録</a></h3>
